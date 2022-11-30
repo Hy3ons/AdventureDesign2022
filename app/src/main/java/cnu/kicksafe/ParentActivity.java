15ba894a194cc16407b780ef73478dc4862a6418
Hy3ons <hhs200306@gmail.com>
2022-11-30 16:16:21 +0900

    16:16

--- app/src/main/java/cnu/kicksafe/ParentActivity.java
+++ app/src/main/java/cnu/kicksafe/ParentActivity.java
@@ -1,14 +1,21 @@
 package cnu.kicksafe;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ParentActivity extends AppCompatActivity {
-
+    private ImageView setting, home, profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
+
+        setting = findViewById(R.id.settingButton);
+        home = findViewById(R.id.homeButton);
+        profile = findViewById(R.id.personButton);
+
+        
     }
 }

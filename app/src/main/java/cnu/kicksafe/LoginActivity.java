155768a6c4700c8892bf965fda0ed7b415d853cb
Hy3ons <hhs200306@gmail.com>
2022-11-30 15:19:00 +0900

    android version update

---
+++ app/src/main/java/cnu/kicksafe/LoginActivity.java
@@ -0,0 +1,16 @@
+package cnu.kicksafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}

601c3ec15d7394d93dd7c6284721178cb08805be
Hy3ons <hhs200306@gmail.com>
2022-12-07 15:46:43 +0900

    22/17/07

---
+++ app/src/main/java/cnu/kicksafe/loginSelect.java
@@ -0,0 +1,14 @@
+package cnu.kicksafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_select);
+    }
+}
